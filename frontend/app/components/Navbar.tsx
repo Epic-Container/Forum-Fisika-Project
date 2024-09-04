fdef7540db7252f24e6f78075a0b4bcfecb13b5b
Epic-Container <smpdrive0@gmail.com>
2024-09-05 00:11:27 +0700

    Styling Navbar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,26 +1,38 @@
 "use client"
 import React from "react";
-import { MdOutlineAccountCircle } from "react-icons/md";
+import { MdOutlineAccountCircle, MdChatBubbleOutline, MdAnnouncement } from "react-icons/md";
 const Navbar = () => {
+
     return (
-        <div className="w-[50px] md:w-[220px] h-screen secondary-dark">
+        <div className="fixed group transtition-all duration-150 w-[60px] md:hover:w-[250px] h-screen secondary-dark rounded-r-lg">
             <div className="flex flex-col py-2 overflow-y-auto items-center gap-4">
-                <h1 className="hidden md:block font-bold border-b-2 p-3 text-xl">
-                    Forum Fisika
-                </h1>
-                <button className="navbar-button trinary-dark">
-                    <MdOutlineAccountCircle />
-                    <span className="navbar-button-text">Account</span>
-                </button>
-                <button>
-                    
-                </button>
-                <button>
-
-                </button>
-                <button>
-
-                </button>
+                <h1 className="md:group-hover:block hidden pt-3 text-2xl">Forum Fisika</h1>
+                <div className="w-full flex flex-col items-center">
+                    <button className="navbar-hover">
+                        <span className="button-child">
+                            <MdAnnouncement className="text-4xl" />
+                            <h1 className="transition-all duration-150 md:group-hover:block hidden">
+                                Announcement
+                            </h1>
+                        </span>
+                    </button>
+                    <button className="navbar-hover">
+                        <span className="button-child">
+                            <MdChatBubbleOutline className="text-4xl" />
+                            <h1 className="transition-all duration-150 md:group-hover:block hidden">
+                                Forum
+                            </h1>
+                        </span>
+                    </button>
+                    <button className="navbar-hover">
+                        <span className="button-child">
+                            <MdOutlineAccountCircle className="text-4xl" />
+                            <h1 className="transition-all duration-150 md:group-hover:block hidden">
+                                Profile
+                            </h1>
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
     );
